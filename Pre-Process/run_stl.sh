95a8fd640b3f2dd820c4b2c90e3ae9bf522d377f
Fabian Hernández <fabian_alexis02@hotmail.com>
2024-07-10 12:16:38 +0200

    Run MN

--- Pre-Process/run_stl.sh
+++ Pre-Process/run_stl.sh
@@ -5,12 +5,15 @@
 #SBATCH --ntasks=224
 #SBATCH --time=02:00:00
 #SBATCH --qos=gp_bsccase
-### Queue and account
 #SBATCH --account=bsc21
-#SBATCH --mail-type=end
+#SBATCH --mail-type=END
 #SBATCH --mail-user=fabian.hernandez@bsc.es
 
+# Activate your virtual environment
+source $VENV
 
-export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+# # Set the number of CPUs per task
+# export SRUN_CPUS_PER_TASK=224  # Set the desired number of CPUs per task
 
-mpirun python ./STL2GeoTool.py
+# Run the Python script with srun
+srun python ./STL2GeoTool_loop.py
