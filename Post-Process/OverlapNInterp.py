3199f13296b7fbd85a14a9c6011c3eb2c5fdc052
Fabian Hernández <fabian_alexis02@hotmail.com>
2024-07-17 18:45:09 +0200

    Post

--- Post-Process/OverlapNInterp.py
+++ Post-Process/OverlapNInterp.py
@@ -3,34 +3,52 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
-from PP_functions import extract_upc_number, read_output_files, interpolate, overlap_matrix,vel_magNdir
+from PP_functions import extract_upc_number, read_output_files, interpolate, overlap_matrix,vel_magNdir,save_matrix_as_image
 
 #### PARAMETERS FROM WIND-NN ####
 p_overlap = 0.5
-step = 256
-y_frames = 5    
-x_frames = 7
-x_dir = x_frames*step
-y_dir = y_frames*step
-overlap = int(p_overlap*step)
-output_dir = './BIM/'
+N_points = 256
+step = int(p_overlap * N_points)
+overlap = N_points - step
+y_frames = 5
+x_frames = 5
+y_dir = y_frames * N_points
+x_dir = x_frames * N_points
+DATASET_PATH='../Wind-NN'
+output_dir = './final_output/output/'
+basename = 'grid_of_cubes'
+x_factor = 1.5
+y_factor = 1.5
 ##################################
 
 if __name__ == '__main__':
-    matrix_U,_ = read_output_files(output_dir, 'UGT',x_frames,y_frames) # In the inference scripts the output writes 'UGT' or 'VGT' for U and V wind fields
-    combined_matrix_U = overlap_matrix(matrix_U, step, overlap, y_dir, x_frames)
-    matrix_V,_ = read_output_files(output_dir, 'VGT',x_frames,y_frames)
-    combined_matrix_V = overlap_matrix(matrix_V, step, overlap, y_dir, x_frames)
-    # Adjust the matrix to remove vide regions
-    combined_matrix_U = combined_matrix_U[y_frames*overlap:, :]
-    combined_matrix_U = combined_matrix_U[:, :-overlap]
-    combined_matrix_V = combined_matrix_V[y_frames*overlap:, :]
-    combined_matrix_V = combined_matrix_V[:, :-overlap]
-    VMAG,VDIR2D = vel_magNdir(combined_matrix_U, combined_matrix_V)
-    # Create the plot
-    fig, ax = plt.subplots()
-    cax = ax.imshow(VMAG, cmap='magma')
-    # Add the colorbar
-    cbar = fig.colorbar(cax, ax=ax)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    matrix_U = read_output_files(DATASET_PATH, 'UGT')
+    overlap_matrix_U = overlap_matrix(matrix_U, N_points, step, overlap, y_dir, x_frames,x_factor, y_factor)
+    matrix_V = read_output_files(DATASET_PATH, 'VGT')
+    overlap_matrix_V = overlap_matrix(matrix_V, N_points, step, overlap, y_dir, x_frames,x_factor,y_factor)
+    VMAG,VDIR = vel_magNdir(overlap_matrix_U, overlap_matrix_V)
+    matrix_mask= read_output_files(DATASET_PATH, 'MASK')
+    mask = overlap_matrix(matrix_mask, N_points, step, overlap, y_dir, x_frames,x_factor,y_factor)
+    
+    # cut_U = overlap_matrix_U[overlap*y_frames-overlap:, 65:768]
+    # cut_V = overlap_matrix_V[overlap*y_frames-overlap:, 65:768]
+    # mask_cut = mask[overlap*y_frames-overlap:, 65:768]
+    # cutcut_U = cut_U[0:600,:]
+    # cutcut_V = cut_V[0:600,:]
+    # mask_cutcut = mask_cut[0:600,:]
+    # VMAG,VDIR = vel_magNdir(cutcut_U, cutcut_V)
+    # mask_multiply = mask_cutcut * VMAG
+    
+    plt.plot(matrix_U)
     plt.show()
+    plt.savefig(f'image.png')
+    
+    save_matrix_as_image(VMAG, output_dir + 'VMAG.png')
+    # save_matrix_as_image(mask_multiply, output_dir + 'VMAG_mask.png')
+    
+    
+    
+
     
--- Post-Process/overlap.py
+++ Post-Process/overlap.py
@@ -1,3 +1,4 @@
+#### GOOOD ONE ######
 import os
 import numpy as np
 import pandas as pd
@@ -5,6 +6,19 @@ import matplotlib.pyplot as plt
 import re
 from PIL import Image
 
+#### PARAMETERS FROM WIND-NN ####
+p_overlap = 0.5
+N_points = 256
+step = int(p_overlap * N_points)
+overlap = N_points - step
+y_frames = 5
+x_frames = 5
+y_dir = y_frames * N_points
+x_dir = x_frames * N_points
+DATASET_PATH='../Wind-NN/output/'
+output_dir = './final_output/'
+basename = 'grid_of_cubes'
+##################################
 def save_matrix_as_image(matrix, output_file,colormap='magma'):
     # Normalize matrix values to 0-1
     normalized_matrix = (matrix - np.min(matrix)) / (np.max(matrix) - np.min(matrix))
@@ -36,13 +50,13 @@ def read_output_files(output_dir, keyword):
                 data = np.genfromtxt(f, delimiter=',')
                 matrix.append(data)
             f.close()
-    # complete_matrix = np.zeros((y_dir, x_dir))
-    # n = 0
-    # for i in range(y_dir - N_points, -1, -N_points):
-    #     for j in range(0, x_dir, N_points):
-    #         complete_matrix[i:i + N_points, j:j + N_points] = matrix[n]
-    #         n += 1
-    return matrix #, complete_matrix  # Complete matrix is the matrix with all the frames, only visualization purposes
+    complete_matrix = np.zeros((y_dir, x_dir))
+    n = 0
+    for i in range(y_dir - N_points, -1, -N_points):
+        for j in range(0, x_dir, N_points):
+            complete_matrix[i:i + N_points, j:j + N_points] = matrix[n]
+            n += 1
+    return matrix, complete_matrix  # Complete matrix is the matrix with all the frames, only visualization purposes
 
 def interpolate(matrix1, matrix2, overlap, axis, factor=1):
     # Generate the interpolation weights using an exponential decay function
@@ -83,4 +97,31 @@ def overlap_matrix(matrix, N_points, step, overlap, y_dir, x_frames,x_factor,y_f
 def vel_magNdir(U, V,W=0):
     VMAG = np.sqrt(U**2 + V**2 + W**2)
     VDIR2D = np.arctan2(V,U)
+    
     return VMAG, VDIR2D
+
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+x_factor = 1.5
+y_factor = 1.5
+matrix_U, matrix = read_output_files(DATASET_PATH, 'UGT') # In the inference scripts the output writes 'UGT' or 'VGT' for U and V wind fields
+overlap_matrix_U = overlap_matrix(matrix_U, N_points, step, overlap, y_dir, x_frames,x_factor, y_factor)
+matrix_V, matrix = read_output_files(DATASET_PATH, 'VGT')
+overlap_matrix_V = overlap_matrix(matrix_V, N_points, step, overlap, y_dir, x_frames,x_factor,y_factor)
+VMAG,VDIR = vel_magNdir(overlap_matrix_U, overlap_matrix_V)
+matrix_mask,_= read_output_files(DATASET_PATH, 'MASK')
+mask = overlap_matrix(matrix_mask, N_points, step, overlap, y_dir, x_frames,x_factor,y_factor)
+
+cut_U = overlap_matrix_U[overlap*y_frames-overlap:, 65:768]
+cut_V = overlap_matrix_V[overlap*y_frames-overlap:, 65:768]
+mask_cut = mask[overlap*y_frames-overlap:, 65:768]
+cutcut_U = cut_U[0:600,:]
+cutcut_V = cut_V[0:600,:]
+mask_cutcut = mask_cut[0:600,:]
+VMAG,VDIR = vel_magNdir(cutcut_U, cutcut_V)
+
+mask_multiply = mask_cutcut * VMAG
+# Save matrix as image
+save_matrix_as_image(VMAG, output_dir + 'VMAG.png')
+save_matrix_as_image(mask_multiply, output_dir + 'VMAG_mask.png')
+
