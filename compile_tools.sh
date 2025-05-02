d2149767ed30b75ee3658ab416c10e947ca0bfc9
Fabian Hernandez <fabian_alexis02@hotmail.com>
2025-05-02 14:40:01 +0200

    city4cfd executable build

--- compile_tools.sh
+++ compile_tools.sh
@@ -4,8 +4,11 @@
 sudo apt-get install libmpfr-dev libgmp-dev libboost-all-dev libeigen3-dev libomp-dev libgdal-dev
 cd City4CFD
 mkdir build && cd build
-cmake .. -DCGAL_DIR=./cgal-6.0.1
+cmake .. -DCGAL_DIR=../cgal-6.0.1
 make -j 4
+cd ../../pre-process/geo4CFD/
+ln -sf ../../City4CFD/build/city4cfd .
+cd ../../
 
 cd ../..
 # Compile pyqvarsi
