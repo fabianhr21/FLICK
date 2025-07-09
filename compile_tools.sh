f98695c59f1a6fc277f62901eb35f0f5bc78a530
fabianhr21 <fabian_alexis02@hotmail.com>
2025-07-09 12:31:11 +0200

    Modified compile_tools

--- compile_tools.sh
+++ compile_tools.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 # Compile city4CFD
-sudo apt-get install libmpfr-dev libgmp-dev libboost-all-dev libeigen3-dev libomp-dev libgdal-dev
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y \
+    libmpfr-dev \
+    libgmp-dev \
+    libboost-all-dev \
+    libeigen3-dev \
+    libomp-dev \
+    libgdal-dev
+
+# Set FLIC to current directory
+export FLIC=$(pwd)
+
+# Compile City4CFD
 cd City4CFD
-mkdir build && cd build
-cmake .. -DCGAL_DIR=../cgal-6.0.1
-make -j 4
+mkdir -p build && cd build
+cmake .. -DCGAL_DIR=$FLIC/City4CFD/cgal-6.0.1
+make -j4
+
+# Create symlink to city4cfd binary
 cd ../../pre-process/geo4CFD/
 ln -sf ../../City4CFD/build/city4cfd .
 cd ../../
 
-cd ../..
 # Compile pyqvarsi
-cd pyqvarsi
-make
+#cd pyqvarsi
+#make
