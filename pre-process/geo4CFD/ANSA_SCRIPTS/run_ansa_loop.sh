5c4de48816d9a8551d33d6cfb232d84641405111
Fabian Hernandez <fabian_alexis02@hotmail.com>
2026-01-14 13:23:03 +0100

    Added ansa loops

--- pre-process/geo4CFD/ANSA_SCRIPTS/run_ansa_loop.sh
+++ pre-process/geo4CFD/ANSA_SCRIPTS/run_ansa_loop.sh
@@ -9,10 +9,12 @@ for d in $input_dir$city/*/ ; do
     [[ "$d" == *"__pycache__"* ]] && continue
     # echo "Processing directory: $d"
     dirname=$(basename "$d")
-    buildings_file="${d}output/${dirname}_Buildings.ansa"
+    buildings_file="${d}output/${dirname}_Buildings.stl"
     echo "Using buildings file: $buildings_file"
+    # Copy geo file from scripts_final
+    cp "${scripts_final}script_gmsh_ParaPC_orden1.geo" "${d}output/${dirname}_Buildings.geo"
     # echo "RUnning Path: '${d}output/'"
-    ~/ANSA/BETA_CAE_Systems24.1/ansa_v24.1.2/ansa64.sh -nogui -noopencl -execscript "args_RbNDivide.py|main('${buildings_file}','${d}output/','${d}output/')"
+    ~/ANSA/BETA_CAE_Systems24.1/ansa_v24.1.2/ansa64.sh -nogui -noopencl -execscript "args_check_flat.py|main('${buildings_file}','${d}output/','${d}output/')"
 done
 
 # FOr makinf the precursor and split2hexa
